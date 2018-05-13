95f47a3c02802f4df573e8511f3174e80c45cf66
hollywater <lxy314453@163.com>
2018-05-13 21:54:23 +0800

    c++ xcrun -sdk iphoneos clang -arch arm64 -rewrite-objc UIViewController+Category.m

---
+++ OCDemo/AOP(aspect oriented program)/UIViewController+Category.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Category.h
+//  OCDemo
+//
+//  Created by hollywater on 2018/5/13.
+//  Copyright © 2018年 greencici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Category)
+
+- (void)showMessage;
+
+@end
---
+++ OCDemo/AOP(aspect oriented program)/UIViewController+Category.m
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Category.m
+//  OCDemo
+//
+//  Created by hollywater on 2018/5/13.
+//  Copyright © 2018年 greencici. All rights reserved.
+//
+
+#import "UIViewController+Category.h"
+
+@implementation UIViewController (Category)
+
+- (void)showMessage {
+    NSLog(@"message is .....");
+}
+
+@end
